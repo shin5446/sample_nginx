4d29dda0a6f0e1fdabef2d3a4e935d7ccd19b897
shin <5446shin@gmail.com>
2019-03-13 12:28:33 +0900

    ユニコーンファイル変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,56 +1,56 @@
-require 'fileutils'
-listen '/tmp/nginx.socket'
-before_fork do |server,worker|
-	FileUtils.touch('/tmp/app-initialized')
-end
-
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
-preload_app true
-
-listen "/tmp/unicorn.sock"
-pid "/tmp/unicorn.pid"
-
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
-
-# ログの出力
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-
-
-# worker_processes 4
-
-# listen '/tmp/unicorn.sock'
-# pid '/tmp/unicorn.pid'
-
-# stderr_path File.expand_path('log/unicorn.log')
-# stdout_path File.expand_path('log/unicorn.log')
-
-# preload_app true
-
 # require 'fileutils'
-# preload_app true
-# timeout 5
-# worker_processes 4
-# listen '/tmp/nginx.socket', backlog: 1024
-
+# listen '/tmp/nginx.socket'
 # before_fork do |server,worker|
 # 	FileUtils.touch('/tmp/app-initialized')
 # end
+
+# worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+# timeout 15
+# preload_app true
+
+# listen "/tmp/unicorn.sock"
+# pid "/tmp/unicorn.pid"
+
+# before_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+#     Process.kill 'QUIT', Process.pid
+#   end
+
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.connection.disconnect!
+# end
+
+# after_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+#   end
+
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.establish_connection
+# end
+
+# # ログの出力
+# stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+# stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+
+worker_processes 4
+
+listen '/tmp/unicorn.sock'
+pid '/tmp/unicorn.pid'
+
+stderr_path File.expand_path('log/unicorn.log')
+stdout_path File.expand_path('log/unicorn.log')
+
+preload_app true
+
+require 'fileutils'
+preload_app true
+timeout 5
+worker_processes 4
+listen '/tmp/nginx.socket', backlog: 1024
+
+before_fork do |server,worker|
+	FileUtils.touch('/tmp/app-initialized')
+end
